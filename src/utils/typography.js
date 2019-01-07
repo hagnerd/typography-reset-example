4d09686cb5456f30da217a01603c0575687bb7be
Matt Hagner <web@matthagner.com>
2019-01-07 10:20:24 -0600

    initial commit

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+import lawtonTheme from 'typography-theme-lawton'
+import './reset.css'
+
+const typography = new Typography({ ...lawtonTheme, incldueNormalize: false })
+
+export default typography
